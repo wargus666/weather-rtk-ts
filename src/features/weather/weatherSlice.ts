000741d21e723bc99d673581d320370b9f0292aa
wargus666 <russianblue2181@gmail.com>
2025-12-29 19:28:22 +0200

    create-async-thunk

--- src/features/weather/weatherSlice.ts
+++ src/features/weather/weatherSlice.ts
@@ -1,13 +1,15 @@
 import {createSlice} from "@reduxjs/toolkit";
 import type {WeatherInfo} from "../../utils/types.ts";
+import {fetchWeather} from "../api/weatherActions.ts";
 
 const weatherSlice = createSlice({
     name: 'weather',
     initialState: {} as WeatherInfo,
-    reducers: {
-        setWeather: (_state, action) => action.payload,
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchWeather.fulfilled, (_state, action) => action.payload)
     }
 })
 
-export const {setWeather} = weatherSlice.actions;
 export default weatherSlice.reducer;
